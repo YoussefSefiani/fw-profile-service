88715cf8b4f34372b2cef5e35368e98619f3db64
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-05 21:48:19 +0200

    web config

--- src/main/java/fw/profileservice/configuration/WebConfiguration.java
+++ src/main/java/fw/profileservice/configuration/WebConfiguration.java
@@ -2,7 +2,6 @@ package fw.profileservice.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,8 +9,10 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
+        registry.addMapping("/**")
+                .exposedHeaders("*")
                 .allowedMethods("*")
-                .exposedHeaders("*");
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*");
     }
 }
