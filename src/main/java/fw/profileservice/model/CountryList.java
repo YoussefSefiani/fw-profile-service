d96174df024123ae29b349403fab8fba61e81b1a
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-06-06 19:58:13 +0200

    host

--- src/main/java/fw/profileservice/model/CountryList.java
+++ src/main/java/fw/profileservice/model/CountryList.java
@@ -1,7 +1,15 @@
 package fw.profileservice.model;
 
 public enum CountryList {
-
-    BELGIUM, FRANCE, NETHERLANDS
-
+    BD, BE, BF, BG, BA, BB, WF, BL, BM, BN, BO, BH, BI, BJ, BT, JM, BW, WS, BR, BS, JE, BY, BZ, RU,
+    RW, RS, TL, RE, TM, TJ, RO, TK, GW, GU, GT, GS, GR, GQ, GP, JP, GY, GG, GF, GE, GD, GB, GA, SV,
+    GN, GM, GL, GI, GH, OM, TN, JO, HR, HT, HU, HK, HN, HM, VE, PR, PS, PW, PT, SJ, PY, IQ, PA, PF,
+    PG, PE, PK, PH, PN, PL, PM, ZM, EH, EE, EG, ZA, EC, IT, VN, SB, ET, SO, ZW, SA, ES, ER, ME, MD,
+    MG, MF, MA, MC, UZ, MM, ML, MO, MN, MH, MK, MU, MT, MW, MV, MQ, MP, MS, MR, IM, UG, TZ, MY, MX,
+    IL, FR, IO, SH, FI, FJ, FK, FM, FO, NI, NL, NO, NA, VU, NC, NE, NF, NG, NZ, NP, NR, NU, CK, XK,
+    CI, CH, CO, CN, CM, CL, CC, CA, CG, CF, CD, CZ, CY, CX, CR, CW, CV, CU, SZ, SY, KG, KE, SS, SR,
+    KH, KN, KM, ST, SK, KR, SI, KP, KW, SN, SM, SL, SC, KZ, KY, SG, SE, SD, DO, DM, DJ, DK, VG, DE,
+    YE, DZ, US, UY, YT, LB, LC, LA, TV, TW, TT, TR, LK, LI, LV, TO, LT, LU, LR, LS, TH, TG, TD, TC,
+    LY, VA, VC, AE, AD, AG, AF, AI, VI, IS, IR, AM, AL, AO, AQ, AS, AR, AU, AT, AW, IN, AX, AZ, IE,
+    ID, UA, QA, MZ
 }
